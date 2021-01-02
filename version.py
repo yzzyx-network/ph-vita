d01bc13533c155f3253f8a5075a1fdecf47d922b
Álex Román Núñez <eirexe123@gmail.com>
2021-01-02 23:54:52 +0100

    PH customizations

--- version.py
+++ version.py
@@ -1,10 +1,10 @@
-short_name = "godot"
-name = "Godot Engine"
+short_name = "projectheartbeat"
+name = "Project Heartbeat"
 major = 3
 minor = 5
 patch = 0
 status = "beta"
 module_config = ""
 year = 2022
-website = "https://godotengine.org"
 docs = "3.4"
+website = "https://eirteam.moe"
