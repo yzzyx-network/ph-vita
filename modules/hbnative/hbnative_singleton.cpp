15046fb0874cd7993582a9c4e464985e01468c47
Álex Román Núñez <eirexe123@gmail.com>
2021-12-14 10:53:42 +0100

    Remove romaji-cpp

--- modules/hbnative/hbnative_singleton.cpp
+++ modules/hbnative/hbnative_singleton.cpp
@@ -1,5 +1,4 @@
 #include "hbnative_singleton.h"
-#include "thirdparty/romaji-cpp/romaji.h"
 
 HBNativeSingleton *HBNativeSingleton::singleton = nullptr;
 
@@ -12,10 +11,5 @@ void HBNativeSingleton::_bind_methods() {
 }
 
 String HBNativeSingleton::romanize(String source) {
-	std::wstring wstr = source.c_str();
-	std::string out_str;
-	japanese::utf8_kana_to_romaji(source.utf8().get_data(), out_str);
-	String str_o;
-	str_o.parse_utf8(out_str.c_str(), out_str.length());
-	return str_o;
+	return source;
 }
